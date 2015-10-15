3b6b1f34f491f1092a0363f5c7da3c3135e0375c
Martin Pettersson <kontakt@martinpettersson.se>
2015-10-15 06:37:30 +0200

    lol

--- arkanoid/main.cpp
+++ arkanoid/main.cpp
@@ -1,91 +1,35 @@
-
 //
-// Disclamer:
-// ----------
+//  p1.cpp
+//  arkanoid
 //
-// This code will work only if you selected window, graphics and audio.
+//  Created by Martin Pettersson on 15/10/15.
+//  Copyright © 2015 Martin Pettersson. All rights reserved.
 //
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resource, use the helper
-// method resourcePath() from ResourcePath.hpp
-//
-
-#include <SFML/Audio.hpp>
+#include <stdio.h>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-// Here is a small helper for you ! Have a look.
-#include "ResourcePath.hpp"
+using namespace std;
+using namespace sf;
 
-int main(int, char const**)
+constexpr int windowWidth{800}, windowHeight{600};
+
+int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        // Clear screen
+    // Creation of the game window
+    RenderWindow window{{windowHeight,windowWidth}, "Arkanoid"};
+    window.setFramerateLimit(60);
+    
+    while (true) {
+        // Clear the window from old graphics
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
+        // Exit the loop if the player presses "Escape"
+        if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) {
+            break;
+        }
+        // Display the contents of the window
         window.display();
     }
-
-    return EXIT_SUCCESS;
 }
+
+
