66f716dd0fdfb6ed62782991aeeec5df1442c535
Martin Pettersson <kontakt@martinpettersson.se>
2015-11-03 15:03:36 +0100

    Increased paddle speed

--- arkanoid/main.cpp
+++ arkanoid/main.cpp
@@ -13,7 +13,7 @@ using namespace sf;
 
 constexpr int windowHeight{600}, windowWidth{800};
 constexpr float ballRadius{10.f}, ballVelocity{11.f};
-constexpr float paddleWidth{100.f}, paddleHeight{10.f}, paddleVelocity{8.f};
+constexpr float paddleWidth{100.f}, paddleHeight{10.f}, paddleVelocity{9.f};
 constexpr float blockWidth{60.f}, blockHeight{20.f};
 constexpr int countBlocksX{11}, countBlocksY{4};
 
